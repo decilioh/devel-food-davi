cc1a6a5b152a0a743b1ff7bb4afc0dd2206f8357
G4rutti <davigarutti5@gmail.com>
2024-08-13 14:02:25 -0300

    Criando a rota esqueci a senha

---
+++ src/Pages/EsqueciSenha/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EsqueciSenha = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default EsqueciSenha
--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,7 +1,9 @@
 //Define a configuração das rotas da aplicação
 
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+
 import Login from "../Pages/Login"
+import EsqueciSenha from "../Pages/EsqueciSenha"
 
 const Router = () => {
   return (
@@ -9,6 +11,7 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<Navigate to="/login"/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/esqueci-senha" element={<EsqueciSenha/>}/>
         </Routes>
     </BrowserRouter>
   )
