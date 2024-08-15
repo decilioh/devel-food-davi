c9b28c540d422484f6057ed0745f38e3cc7a25bb
Gabriel Decilio <gdhonorio@rd.com.br>
2024-08-15 16:06:26 -0300

    [develop] - route login

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,16 +1,24 @@
 //Define a configuração das rotas da aplicação
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Login from "../Pages/Login"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "../Pages/Login";
 
 const Router = () => {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/login"  element={<Login/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/teste"
+          element={
+            <>
+              <h1>Teste</h1>
+            </>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
