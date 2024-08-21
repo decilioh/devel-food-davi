f9abee41bfb8b2ae33946655c41c05ee61bab6be
G4rutti <davigarutti5@gmail.com>
2024-08-21 10:20:46 -0300

    ajeitar algumas coisas

--- src/Pages/EsqueciSenha/EsqueciSenha.styles.ts
+++ src/Pages/EsqueciSenha/EsqueciSenha.styles.ts
@@ -5,7 +5,6 @@ export const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: white;
   height: 100vh;
   overflow-y: hidden;
 `
