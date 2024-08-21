a3f9e3f8e075b2d3ab244313d88882dd433fbb6b
G4rutti <davigarutti5@gmail.com>
2024-08-21 09:07:57 -0300

    Cor de fundo diferente do Figma

--- src/Pages/EsqueciSenha/EsqueciSenha.styles.ts
+++ src/Pages/EsqueciSenha/EsqueciSenha.styles.ts
@@ -5,7 +5,8 @@ export const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 20vh;
+  background-color: white;
+  height: 100vh;
 `
 
 export const ImageLogo = styled.img`
