29c424a8c0b37d240775b13fc1d22acbf43345b8
G4rutti <davigarutti5@gmail.com>
2024-08-27 12:01:35 -0300

    Loader ✅

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,13 +3,15 @@ import styled from "styled-components"
 import { useContext } from "react";
 import { ThemeContext } from "../../context/themeContext";
 import HeaderMain from "../../components/layout/Header";
-
+import Loader from "../../components/common/Loader";
 
 
 const Home = () => {
+    const theme = useContext(ThemeContext)
     return (
         <>
             <HeaderMain />
+            {/* <Loader theme={theme}/> */}
         </>
     )
 }
