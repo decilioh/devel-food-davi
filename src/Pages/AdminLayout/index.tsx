f45716561df06f31afef232f66f7a55e922dae3d
G4rutti <davigarutti5@gmail.com>
2024-08-29 16:01:14 -0300

    responsivo agora

--- src/Pages/AdminLayout/index.tsx
+++ src/Pages/AdminLayout/index.tsx
@@ -1,55 +1,36 @@
 import styled from "styled-components"
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../context/themeContext";
 import HeaderMain from "../../components/layout/Header";
 import { ModalContext } from "../../context/modalContext";
 import Sidebar from "../../components/layout/Sidebar";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { Content, MainContainer, MainSection } from "./AdminLayout.styles";
 
-const MainContainer = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  overflow-y: hidden;
-  @media screen and (max-width: 1025px){
-    overflow-y: auto;
-  }
-  
-`;
 
-const MainSection = styled.div`
-  display: flex;
-  flex: 1;
-`;
-
-const Content = styled.div`
-  background-color: ${({ theme }) => theme.background};
-  overflow-y: hidden;
-  max-height: 1080px;
-  overflow-x: hidden;
-
-  width: 100%;
-  margin: auto 0 auto 99px;
-  @media screen and (max-width: 1025px){
-    overflow-y: auto;
-    margin: auto 0 auto auto;
-
-  }
-  
-`;
 
 
 
 const Layout = () => {
   const theme = useContext(ThemeContext)
   const modalContext = useContext(ModalContext)
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 650);
+
+  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 650);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <MainContainer>
-      <HeaderMain />
+      <HeaderMain toggleSidebar={toggleSidebar} />
       <MainSection>
-        <Sidebar />
+        <Sidebar isMobile={isMobile} toggleSidebar={toggleSidebar} isOpen={isSidebarOpen} />
         <Content>
           <Outlet />
         </Content>
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,14 +1,25 @@
-import { useContext } from 'react'
-import { ImageLogoHeader, HeaderHome } from './Header.styles'
-import { ThemeContext } from '../../../context/themeContext'
-import LogoDevelHeaderWhite from "../../../assets/images/LogoDevelHeaderWhite.png"
-import LogoDevelHeaderBlack from "../../../assets/images/LogoDevelHeaderBlack.png"
+import { useContext } from 'react';
+import { ImageLogoHeader, HeaderHome, MenuButton } from './Header.styles';
+import { ThemeContext } from '../../../context/themeContext';
+import LogoDevelHeaderWhite from "../../../assets/images/LogoDevelHeaderWhite.png";
+import LogoDevelHeaderBlack from "../../../assets/images/LogoDevelHeaderBlack.png";
+import { FaBars } from 'react-icons/fa';
 
-const HeaderMain = () => {
-    const theme = useContext(ThemeContext)
-    return (
-        <HeaderHome><ImageLogoHeader src={theme?.theme === "light" ? LogoDevelHeaderWhite : LogoDevelHeaderBlack} /></HeaderHome>
-    )
+interface HeaderMainProps {
+  toggleSidebar: () => void;
 }
 
-export default HeaderMain
+const HeaderMain: React.FC<HeaderMainProps> = ({ toggleSidebar }) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <HeaderHome>
+            <MenuButton onClick={toggleSidebar}>
+                <FaBars />
+            </MenuButton>
+            <ImageLogoHeader src={theme?.theme === "light" ? LogoDevelHeaderWhite : LogoDevelHeaderBlack} />
+        </HeaderHome>
+    );
+}
+
+export default HeaderMain;
