c617db5ef67625e1839ad7388d1604160d039984
G4rutti <davigarutti5@gmail.com>
2024-09-17 16:11:12 -0300

    🧪 Adicionando alguns testes unitários

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+      '^.+\\.(js|jsx|mjs)$': 'babel-jest',
+    },
+    setupFiles: ['./jest.setup.js'],
+  };
+  
