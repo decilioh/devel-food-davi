eab0ab65730c1aa0db9dc19925878f6ff707968f
G4rutti <davigarutti5@gmail.com>
2024-09-27 09:05:24 -0300

    adicionando o env

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuwEq_Jc3buD1sEXXFzOTSJ1nqs2hSXoM",
-  authDomain: "bozeca-123a8.firebaseapp.com",
-  projectId: "bozeca-123a8",
-  storageBucket: "bozeca-123a8.appspot.com",
-  messagingSenderId: "143342716551",
-  appId: "1:143342716551:web:c3a7eee59c34c606bcda22"
+  apiKey: process.env.VITE_BASE_URL_API_KEY,
+  authDomain: process.env.VITE_BASE_URL_AUTH_DOMAIN,
+  projectId: process.env.VITE_BASE_URL_PROJECT_ID,
+  storageBucket: process.env.VITE_BASE_URL_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_BASE_URL_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_BASE_URL_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
